e98d5d4cfe3a70b41d03f4bd47c296d2480031d3
GON <GON@APOLO.lan>
2013-11-24 00:17:48 -0600

    FUNCIONES BUSCAR COPIAR CORTAR PEGAR IMPRIMIR GUARDAR Y GUARDAR COMO

--- src/Pantalla.java
+++ src/Pantalla.java
@@ -37,11 +37,13 @@ private JLabel Tamaño; //Guarda el tamaño que adquiere el archivo
 private UndoManager undoManager;            //instancia de UndoManager (administrador de edición)
 public JFileChooser fc = new JFileChooser(); 
 int state;
+private String UltimaBusqueda = "";
 
 
 
     public Pantalla() {
         initComponents();
+        //Siguiente.enable(false);
        
         
     }
@@ -78,11 +80,23 @@ this.Tamaño.setText(siz);}
         BCopiar = new javax.swing.JButton();
         BCortar = new javax.swing.JButton();
         BPegar = new javax.swing.JButton();
+        Siguiente = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         Texto = new javax.swing.JTextArea();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jSeparator5 = new javax.swing.JPopupMenu.Separator();
+        BMImprimir = new javax.swing.JMenuItem();
+        BMSalir = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        BMBuscar = new javax.swing.JMenuItem();
+        jSeparator4 = new javax.swing.JPopupMenu.Separator();
+        BMCopiar = new javax.swing.JMenuItem();
+        BMCortar = new javax.swing.JMenuItem();
+        BMPegar = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -123,6 +137,11 @@ this.Tamaño.setText(siz);}
         BGuardar.setFocusable(false);
         BGuardar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BGuardar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BGuardarActionPerformed(evt);
+            }
+        });
         BarraHerramientas.add(BGuardar);
 
         BGuardarC.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/tp_saveas.png"))); // NOI18N
@@ -145,6 +164,11 @@ this.Tamaño.setText(siz);}
         BImprimir.setFocusable(false);
         BImprimir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BImprimir.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BImprimir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BImprimirActionPerformed(evt);
+            }
+        });
         BarraHerramientas.add(BImprimir);
         BarraHerramientas.add(jSeparator3);
 
@@ -171,6 +195,11 @@ this.Tamaño.setText(siz);}
         BCopiar.setFocusable(false);
         BCopiar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BCopiar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BCopiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BCopiarActionPerformed(evt);
+            }
+        });
         BarraHerramientas.add(BCopiar);
 
         BCortar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/tp_cut.png"))); // NOI18N
@@ -179,6 +208,11 @@ this.Tamaño.setText(siz);}
         BCortar.setFocusable(false);
         BCortar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BCortar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BCortar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BCortarActionPerformed(evt);
+            }
+        });
         BarraHerramientas.add(BCortar);
 
         BPegar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/tp_paste.png"))); // NOI18N
@@ -187,16 +221,103 @@ this.Tamaño.setText(siz);}
         BPegar.setFocusable(false);
         BPegar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         BPegar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BPegar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BPegarActionPerformed(evt);
+            }
+        });
         BarraHerramientas.add(BPegar);
+
+        Siguiente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/tp_next.png"))); // NOI18N
+        Siguiente.setBorder(null);
+        Siguiente.setFocusable(false);
+        Siguiente.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        Siguiente.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        Siguiente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SiguienteActionPerformed(evt);
+            }
+        });
+        BarraHerramientas.add(Siguiente);
 
         Texto.setColumns(20);
         Texto.setRows(5);
         jScrollPane1.setViewportView(Texto);
 
         jMenu1.setText("Archivo");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Abrir");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem2.setText("Guardar");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem3.setText("Guardar Como");
+        jMenu1.add(jMenuItem3);
+        jMenu1.add(jSeparator5);
+
+        BMImprimir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_I, java.awt.event.InputEvent.CTRL_MASK));
+        BMImprimir.setText("Imprimir");
+        BMImprimir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMImprimirActionPerformed(evt);
+            }
+        });
+        jMenu1.add(BMImprimir);
+
+        BMSalir.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.CTRL_MASK));
+        BMSalir.setText("Salir");
+        BMSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMSalirActionPerformed(evt);
+            }
+        });
+        jMenu1.add(BMSalir);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Edición");
+
+        BMBuscar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_B, java.awt.event.InputEvent.CTRL_MASK));
+        BMBuscar.setText("Buscar");
+        BMBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMBuscarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(BMBuscar);
+        jMenu2.add(jSeparator4);
+
+        BMCopiar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_MASK));
+        BMCopiar.setText("Copiar");
+        BMCopiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMCopiarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(BMCopiar);
+
+        BMCortar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_X, java.awt.event.InputEvent.CTRL_MASK));
+        BMCortar.setText("Cortar");
+        BMCortar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMCortarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(BMCortar);
+
+        BMPegar.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_P, java.awt.event.InputEvent.CTRL_MASK));
+        BMPegar.setText("Pegar");
+        BMPegar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BMPegarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(BMPegar);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -255,9 +376,149 @@ this.Tamaño.setText(siz);}
     }//GEN-LAST:event_BAbrirActionPerformed
 
     private void BGuardarCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BGuardarCActionPerformed
-        // TODO add your handling code here:
+        guardarcomo();
     }//GEN-LAST:event_BGuardarCActionPerformed
 
+    private void BMBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMBuscarActionPerformed
+        busca();
+    }//GEN-LAST:event_BMBuscarActionPerformed
+
+    private void SiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SiguienteActionPerformed
+        if (UltimaBusqueda.length() > 0) {    //si la última búsqueda contiene texto
+            String textAreaContent = Texto.getText();    //se obtiene todo el contenido del área de edición
+            int pos = Texto.getCaretPosition();    //se obtiene la posición del cursor sobre el área de edición
+            //buscando a partir desde la posición del cursor, se obtiene la posición de la primera ocurrencia del texto
+            pos = textAreaContent.indexOf(UltimaBusqueda, pos);
+ 
+            if (pos > -1) {    //si la posición es mayor a -1 significa que la búsqueda fue positiva
+                //selecciona el texto en el área de edición para resaltarlo
+                Texto.select(pos, pos + UltimaBusqueda.length());
+            }
+        } else {    //si la última búsqueda no contiene nada
+            busca();    //invoca el método actionSearch()
+        }
+    }//GEN-LAST:event_SiguienteActionPerformed
+
+    private void BCortarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BCortarActionPerformed
+         Texto.cut();
+    }//GEN-LAST:event_BCortarActionPerformed
+
+    private void BCopiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BCopiarActionPerformed
+        Texto.copy();
+    }//GEN-LAST:event_BCopiarActionPerformed
+
+    private void BPegarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BPegarActionPerformed
+        Texto.paste();
+    }//GEN-LAST:event_BPegarActionPerformed
+
+    private void BMCopiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMCopiarActionPerformed
+        Texto.copy();
+    }//GEN-LAST:event_BMCopiarActionPerformed
+
+    private void BMCortarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMCortarActionPerformed
+        Texto.cut();
+    }//GEN-LAST:event_BMCortarActionPerformed
+
+    private void BMPegarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMPegarActionPerformed
+        Texto.paste();
+    }//GEN-LAST:event_BMPegarActionPerformed
+
+    private void BImprimirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BImprimirActionPerformed
+        imprimir();
+    }//GEN-LAST:event_BImprimirActionPerformed
+
+    private void BGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BGuardarActionPerformed
+        if (ArchivoActual == null) {    //si no hay un archivo asociado al documento actual
+            guardarcomo();    //invoca el método actionSaveAs()
+        } else {    //si el documento esta marcado como modificado
+            try {
+                //abre un flujo de datos hacia el archivo asociado al documento actual
+                BufferedWriter bw = new BufferedWriter(new FileWriter(ArchivoActual));
+                //escribe desde el flujo de datos hacia el archivo
+                Texto.write(bw);
+                bw.close();    //cierra el flujo
+ 
+                
+            } catch (IOException ex) {    //en caso de que ocurra una excepción
+                //presenta un dialogo modal con alguna información de la excepción
+                JOptionPane.showMessageDialog(this,
+                                              ex.getMessage(),
+                                              ex.toString(),
+                                              JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_BGuardarActionPerformed
+
+    private void BMSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMSalirActionPerformed
+    this.dispose();
+    }//GEN-LAST:event_BMSalirActionPerformed
+
+    private void BMImprimirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMImprimirActionPerformed
+    imprimir(); 
+    }//GEN-LAST:event_BMImprimirActionPerformed
+public void imprimir(){
+            boolean result = false;    //resultado de la impresión, por defecto es false
+ 
+        //si el documento actual no esta vacío
+        if (Texto.getText().trim().equals("") == false) {
+            //invoca nuestra la clase PrintAction para presentar el dialogo de impresión
+            result = PrintAction.print(Texto, this);
+        }
+}
+
+public void guardarcomo(){
+        SelectorArchivo();    //obtiene un JFileChooser
+ 
+        //presenta un dialogo modal para que el usuario seleccione un archivo
+        state = fc.showSaveDialog(this);
+        if (state == JFileChooser.APPROVE_OPTION) {    //si elige guardar en el archivo
+            File f = fc.getSelectedFile();    //obtiene el archivo seleccionado
+ 
+            try {
+                //abre un flujo de datos hacia el archivo asociado seleccionado
+                BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+                //escribe desde el flujo de datos hacia el archivo
+                Texto.write(bw);
+                bw.close();    //cierra el flujo
+ 
+                //nuevo título de la ventana con el nombre del archivo guardado
+                this.setTitle("Editor de Orlando - " + f.getName());
+ 
+                
+                //establece el archivo guardado como el archivo actual
+                ArchivoActual=(f);
+                
+            } catch (IOException ex) {    //en caso de que ocurra una excepción
+                //presenta un dialogo modal con alguna información de la excepción
+                JOptionPane.showMessageDialog(this,
+                                              ex.getMessage(),
+                                              ex.toString(),
+                                              JOptionPane.ERROR_MESSAGE);
+            }
+        }
+}    
+    
+public void busca(){
+String text = JOptionPane.showInputDialog(
+                this,
+                "Texto:",
+                "EditorOrlando - Buscar",
+                JOptionPane.QUESTION_MESSAGE);
+ 
+        if (text != null) {    //si se introdujo texto (puede ser una cadena vacía)
+            String textAreaContent = Texto.getText();    //obtiene todo el contenido del área de edición
+            int pos = textAreaContent.indexOf(text);    //obtiene la posición de la primera ocurrencia del texto
+ 
+            if (pos > -1) {    //si la posición es mayor a -1 significa que la búsqueda fue positiva
+                //selecciona el texto en el área de edición para resaltarlo
+                Texto.select(pos, pos + text.length());
+            }
+ 
+            //establece el texto buscado como el texto de la última búsqueda realizada
+            UltimaBusqueda = text;
+        }
+}
+    
 public void SelectorArchivo(){
    FileFilter textFileFilter = new FileFilter() { //genera un filtro para abrir 
        public boolean accept(File f) {
@@ -324,17 +585,29 @@ public void SelectorArchivo(){
     private javax.swing.JButton BGuardar;
     private javax.swing.JButton BGuardarC;
     private javax.swing.JButton BImprimir;
+    private javax.swing.JMenuItem BMBuscar;
+    private javax.swing.JMenuItem BMCopiar;
+    private javax.swing.JMenuItem BMCortar;
+    private javax.swing.JMenuItem BMImprimir;
+    private javax.swing.JMenuItem BMPegar;
+    private javax.swing.JMenuItem BMSalir;
     private javax.swing.JButton BNuevo;
     private javax.swing.JButton BPegar;
     private javax.swing.JToolBar BarraHerramientas;
+    private javax.swing.JButton Siguiente;
     public javax.swing.JTextArea Texto;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JToolBar.Separator jSeparator1;
     private javax.swing.JToolBar.Separator jSeparator2;
     private javax.swing.JToolBar.Separator jSeparator3;
+    private javax.swing.JPopupMenu.Separator jSeparator4;
+    private javax.swing.JPopupMenu.Separator jSeparator5;
     // End of variables declaration//GEN-END:variables
 
     
